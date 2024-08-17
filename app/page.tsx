998a95efa267f1509f7b11ce09ed070837485e3f
sanaullah <sanaullahjutt146@gmaim.com>
2024-08-17 22:31:37 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>helllooooo jeeeee</h1>
+      <h5>ya h5 ya</h5>
+      <h2>jutt ssss</h2>
+    </main>
+  );
+}
